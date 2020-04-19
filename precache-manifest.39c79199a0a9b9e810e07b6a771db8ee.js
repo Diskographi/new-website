9bf57f8138e7d75f93d1768895732af656409cdd
Cody Hill <william.cody.hill@gmail.com>
2020-04-18 22:59:25 -0700

    Updates

---
+++ precache-manifest.39c79199a0a9b9e810e07b6a771db8ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09930c0750fd3a2a765a267237169792",
+    "url": "/new-website/index.html"
+  },
+  {
+    "revision": "c07ab766bb05122cb8c9",
+    "url": "/new-website/static/css/2.40a775a0.chunk.css"
+  },
+  {
+    "revision": "7dfc54f59f3051265005",
+    "url": "/new-website/static/css/main.ed29d1e2.chunk.css"
+  },
+  {
+    "revision": "c07ab766bb05122cb8c9",
+    "url": "/new-website/static/js/2.4f1918fe.chunk.js"
+  },
+  {
+    "revision": "82fd468f0b880d2c0e11fd4a0bc6714b",
+    "url": "/new-website/static/js/2.4f1918fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dfc54f59f3051265005",
+    "url": "/new-website/static/js/main.c5fd30d7.chunk.js"
+  },
+  {
+    "revision": "49f892f26e2e1bdbb1a5",
+    "url": "/new-website/static/js/runtime-main.da8d13fe.js"
+  }
+]);
